7087554d2a7f8422d1a33fc89a5c2fb35fe14b90
Juncarlo <carlocabangcala@gmail.com>
2018-09-10 15:31:17 +1000

    add files

---
+++ animation.js
@@ -0,0 +1,75 @@
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100){
+            $(".navbar").css({"opacity" : "0"})
+        }
+else {
+        $(".navbar").css({"opacity" : "1"})
+    }
+})
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 300){
+            $(".first-fadeout").css({"opacity" : "0"})
+        }
+else {
+        $(".first-fadeout").css({"opacity" : "1"})
+    }
+})
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 200){
+            $(".bs-example").css({"opacity" : "0"})
+        }
+else {
+        $(".bs-example").css({"opacity" : "1"})
+    }
+})
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 500){
+            $(".second-fadeout").css({"opacity" : "0"})
+        }
+else {
+        $(".second-fadeout").css({"opacity" : "1"})
+    }
+})
+})
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 600){
+            $(".third-fadeout").css({"opacity" : "0"})
+        }
+else {
+        $(".third-fadeout").css({"opacity" : "1"})
+    }
+})
+})
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 900){
+            $(".last-fadeout").css({"opacity" : "0"})
+        }
+else {
+        $(".last-fadeout").css({"opacity" : "1"})
+    }
+})
+})
+
+$(document).ready(function() {
+$(".typewriter-effect").typer({
+strings: [
+"RD IMPRENTA DIGITAL PRINTING"
+
+] });
+});
+
+    
